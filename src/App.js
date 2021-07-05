37a3cc4cc8e9e1a0f6fe851bb3a6ed36078c1478
Alex On <shuhia.on@gmail.com>
2021-07-05 06:21:20 +0200

    Removed link from logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -13,19 +13,29 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            <a className="navbar-brand" href="#" target="_blank">
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="CodingTheSmartWay.com"
+              />
             </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <Link to="/" className="navbar-brand">
+              MERN-Stack Todo App
+            </Link>
             <div className="collpase nav-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
                 </li>
                 <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
                 </li>
               </ul>
             </div>
